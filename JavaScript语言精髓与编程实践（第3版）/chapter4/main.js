0d44c94db0c5e1150265d2fac9c8178d51845f94
zhaochangyuan <757831625@qq.com>
2021-01-13 08:18:23 +0800

    4.6

--- JavaScript语言精髓与编程实践（第3版）/chapter4/main.js
+++ JavaScript语言精髓与编程实践（第3版）/chapter4/main.js
@@ -73,11 +73,22 @@ switch(x) {
     console.log(`${code}: ${message}`);
 } */
 
-let x = 100;
+/* let x = 100;
 y = 200;
 
 console.log(x, y);
 console.log(global.x);
 console.log(global.y);
 console.log(global);
-console.log((new Function('return this'))());
+console.log((new Function('return this'))()); */
+
+let obj = {
+    [Symbol()]: function() {
+        console.log('unnamed symbol method');
+    }
+}
+
+console.log(Object.keys(obj));
+console.log(Object.getOwnPropertySymbols(obj));
+let f = obj[Object.getOwnPropertySymbols(obj)[0]];
+f.call(obj);
