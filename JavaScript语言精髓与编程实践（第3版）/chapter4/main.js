633c3ae4ef70d4d17b23ca02a34cb716b6cfcb9c
zhaochangyuan <757831625@qq.com>
2021-01-13 10:29:09 +0800

    4.8跳过

--- JavaScript语言精髓与编程实践（第3版）/chapter4/main.js
+++ JavaScript语言精髓与编程实践（第3版）/chapter4/main.js
@@ -82,7 +82,7 @@ console.log(global.y);
 console.log(global);
 console.log((new Function('return this'))()); */
 
-let obj = {
+/* let obj = {
     [Symbol()]: function() {
         console.log('unnamed symbol method');
     }
@@ -91,4 +91,59 @@ let obj = {
 console.log(Object.keys(obj));
 console.log(Object.getOwnPropertySymbols(obj));
 let f = obj[Object.getOwnPropertySymbols(obj)[0]];
-f.call(obj);
+f.call(obj); */
+
+/* function foo() {
+    
+}
+
+console.log(global); */
+
+/* console.log('hoisting x:', x);
+var x = 100;
+console.log('value x:', x);
+
+eval('var y = 100');
+console.log('dynamic define y:', y);
+
+z = 100;
+console.log('global z:', global.z); */
+
+/* function f() {
+    var x = 100;
+    eval('var y = 1000');
+    console.log('values if x,y:', x, y);
+}
+f() */
+
+
+/* var x = 100;
+
+y = 200;
+
+eval('var z = 300');
+console.log(global.hasOwnProperty('y'));
+console.log(global.hasOwnProperty('x'));
+
+console.log(delete y, typeof y);
+console.log(delete x, x);
+
+console.log(global);
+
+console.log('------------------');
+var desc = Object.getOwnPropertyDescriptor(global, 'z');
+console.log(desc); */
+
+/* "use strict";
+
+(0, eval)('var a = 1000');
+
+
+function f2() {
+    "use strict";
+    (0, eval)('var b = 2000');
+}
+
+f2();
+console.log(a, b); */
+
