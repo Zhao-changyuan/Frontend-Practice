5f4e3c21e7c1dc800467ef3f050f4b6eb7966cca
zhaochangyuan <757831625@qq.com>
2021-01-14 17:16:43 +0800

    1

--- JavaScript语言精髓与编程实践（第3版）/chapter5/main.js
+++ JavaScript语言精髓与编程实践（第3版）/chapter5/main.js
@@ -124,11 +124,205 @@ foo('test.txt'); */
 }
 foo(undefined, 100 ,200, 300) */
 
-function print(msg) {
+/* function print(msg) {
     console.log(msg);
     console.log(arguments);
 }
 
 var i = 100;
 print(i+=20, i*=2, 'value: ' + i);
-print(i);
+print(i); */
+
+/* function foo() {
+    function func() {
+
+    }
+
+    if (true) {
+        function func2() {
+
+        }
+    }
+
+    console.log(typeof foo, typeof func, typeof func2);
+}
+
+foo();
+console.log(typeof foo, typeof func, typeof func2); */
+
+/* var f = function func2() {
+    console.log(typeof func2);
+}
+
+f();
+
+console.log(typeof func2); */
+
+/* var func2 = function() {
+    console.log(typeof func2);
+};
+
+func2();
+
+x = func2;
+func2 = 'a string';
+x(); */
+
+/* var x = function() {}
+var y = function foo() {}
+
+console.log(x.name, y.name);
+
+delete x.name;
+Object.defineProperty(y, 'name', { value: 'y' });
+console.log(x.name);
+console.log(y.name); */
+
+/* function* myGenerator(x=0, y=0, z=0) {
+    yield x* 2 + y * z;
+}
+
+var tor = myGenerator(1,2,3);
+
+console.log(tor instanceof myGenerator); */
+
+/* var obj = new Object;
+function* f() {
+    console.log('this is <obj>:', this === obj);
+}
+
+var tor = f.call(obj);
+tor.next();
+
+var tor2 = f();
+tor2.next(); */
+
+/* function* myGenerator2(x) {
+    console.log('in generator:', yield 2*x);
+    console.log('again:', [yield]);
+}
+
+var tor = myGenerator2(10);
+console.log('yield Value:', tor.next('1st ignored').value);
+
+tor.next('data...')
+
+tor.next(1, 2, 3, 4) */
+
+/* class MyClass {
+    constructor() {
+        console.log(new.target === MyClass);
+    }
+}
+
+x = new MyClass;
+
+try {
+    MyClass();
+} catch (error) {
+    console.log(error.message);
+} */
+
+/* var obj = {
+    MyClass: class {
+        get value() {
+            return 10;
+        }
+    }
+}
+console.log((new obj.MyClass).value); */
+
+/* var obj2 = {
+    get name() {
+        return 'property getter'
+    },
+
+    async callMe() {},
+
+    *maker() {}
+};
+
+console.log('prototype' in obj2.maker); */
+
+/* var obj3 = {
+    callMe() {
+        console.log(typeof callMe);
+    },
+
+    *callMe2() {
+        console.log(typeof callMe2);
+    }
+};
+
+obj3.callMe();
+obj3.callMe2().next(); */
+
+/* function foo() {
+    var arrowFunc = () => this.name;
+    var obj2 = { name: 'me' };
+    console.log('call:', arrowFunc.call(obj2));
+    console.log('bind:', arrowFunc.bind(obj2)());
+}
+
+var obj = { name: 'object <obj>' };
+foo.call(obj); */
+
+/* class MyFunc {
+    static foo() {
+        console.log('prototype method in MyFunc');
+    }
+}
+
+class MyFuncEx extends MyFunc {
+    static foo() {
+        console.log('own method in MyFuncEx');
+    }
+
+    callMe() {
+        console.log('call me in MyFuncEx');
+    }
+}
+
+var f = MyFuncEx.bind();
+MyFuncEx.foo();
+f.foo();
+
+console.log('prototype' in Object.getPrototypeOf(MyFunc));
+
+// class MyFunc2 extends MyFunc.bind() {}
+
+class MyFunc3 extends MyFuncEx.bind() {
+}
+
+class MyFunc4 extends MyFuncEx {}
+
+(new MyFunc4).callMe();
+
+console.log('callMe' in Object.getPrototypeOf(MyFunc4.prototype));
+
+
+console.log('callMe' in Object.getPrototypeOf(MyFunc3.prototype));
+
+console.log('callMe' in new MyFunc3); */
+
+/* function foo() {
+    'use strict';
+    console.log('this: ', this && typeof this);
+    console.log('args: ', ...arguments);
+}
+
+f = foo.bind(null, 1, 2, 3);
+f(4,5,6); */
+
+/* function MyObject(src) {
+    console.log('Construct by', src);
+    console.log(Object.getPrototypeOf(this) === MyObject.prototype);
+    console.log(new.target === MyObject);
+}
+
+new MyObject('MyObject');
+X = MyObject.bind(null, 'bound MyObject');
+new X; */
+
+
+
