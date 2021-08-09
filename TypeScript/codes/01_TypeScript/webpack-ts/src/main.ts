1ad02ebb37d8e37e4f326cc98d754d474999ec10
zhaochangyuan <757831625@qq.com>
2021-08-09 09:39:12 +0800

    1

--- TypeScript/codes/01_TypeScript/webpack-ts/src/main.ts
+++ TypeScript/codes/01_TypeScript/webpack-ts/src/main.ts
@@ -1,13 +1,89 @@
 
-
-// 接口
+// 泛型
 void function () {
 
-  class Person {
-    constructor(readonly name: string = '大甜甜') {}
+  /* function createArray2<T>(value: T, count: number): T[] {
+    const arr: T[] = []
+    for (let i = 0; i < count; i++) {
+      arr.push(value)
+    }
+
+    return arr;
   }
 
-  const person = new Person();
-  console.log(person)
-  console.log(new Person('牛魔王'))
+  const arr3 = createArray2('33', 4)
+  console.log(arr3[0].split(''))
+  console.log(createArray2(1, 4)) */
+
+  /* function swap<K, V> (a: K, b: V): [V, K] {
+    return [b, a]
+  }
+
+  console.log(swap('abc', 123)) */
+
+  /* interface IBaseCRUD<T> {
+    data: T[]
+    add: (t: T) => void
+    getById: (id: number) => T | undefined
+  }
+
+  class User {
+    id?: number;
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  }
+
+  class UserCRUD implements IBaseCRUD<User> {
+    data: User[] = [];
+
+    add(user: User): void {
+      user = {...user, id:Date.now()}
+      this.data.push(user)
+      console.log('保存user', user.id)
+    }
+    getById(id: number) {
+      return this.data.find(item => item.id === id)
+    }
+  }
+
+  const userCRUD = new UserCRUD()
+  userCRUD.add(new User('tom', 12))
+  userCRUD.add(new User('tom2', 13))
+  console.log(userCRUD.data) */
+
+  // class GenericNumber<T> {
+  //   zeroValue: T
+  //   add: (x: T, y: T) => T
+  // }
+
+  // let myGenericNumber = new GenericNumber<number>()
+  // myGenericNumber.zeroValue = 0
+  // myGenericNumber.add = function (x, y) {
+  //   return x + y
+  // }
+
+  // let myGenericString = new GenericNumber<string>()
+  // myGenericString.zeroValue = 'abc'
+  // myGenericString.add = function (x, y) {
+  //   return x + y
+  // }
+
+  // console.log(myGenericString.add(myGenericString.zeroValue, 'test'))
+  // console.log(myGenericNumber.add(myGenericNumber.zeroValue, 123))
+
+  /* interface Lengthwise {
+    length: number;
+  }
+
+  function fn2<T extends Lengthwise>(x: T): void {
+    console.log(x.length)
+  }
+
+  console.log(fn2('3434')) */
+  jQuery('333')
 }()
