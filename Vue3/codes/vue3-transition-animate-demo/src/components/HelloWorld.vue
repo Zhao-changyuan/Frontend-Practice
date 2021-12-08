63d7bb5ff715ec5b587034a251521587dd4d8e10
Changyuan Zhao <757831625@qq.com>
2021-12-08 18:34:44 +0800

    1

--- Vue3/codes/vue3-transition-animate-demo/src/components/HelloWorld.vue
+++ Vue3/codes/vue3-transition-animate-demo/src/components/HelloWorld.vue
@@ -1,44 +1,52 @@
 <template>
   <div>
-    <transition name="no-mode-fade" mode="out-in">
-      <button v-if="on" key="on" @click="on = false">on</button>
-      <button v-else="on" key="off" @click="on = true">off</button>
-    </transition>
+    <h3>Hover us.</h3>
+    <main>
+      <app-child>
+        <img src='https://images.unsplash.com/photo-1520182205149-1e5e4e7329b4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='image of a woman on a train'>
+      </app-child>
+      <app-child>
+        <img src='https://images.unsplash.com/photo-1501421018470-faf26f6b1bef?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='drawing of a woman sharing soda with a zombie'>
+      </app-child>
+    </main>
   </div>
 </template>
 
 <script lang="ts" setup>
 
-import {ref} from 'vue'
-
-const on = ref(false)
-
 </script>
 <style scoped>
 
-.no-mode-fade-enter-active,
-.no-mode-fade-leave-active {
-  transition: opacity .5s;
-}
-
-.no-mode-fade-enter-from,
-.no-mode-fade-leave-to {
-  opacity: 0;
-}
-
 button {
-  background: #05ae7f;
+  font-family: "Bitter";
+  background: #c62735;
+  color: white;
+  border: 0;
+  padding: 5px 15px;
+  margin: 0 10px;
   border-radius: 4px;
-  display: inline-block;
-  border: none;
-  padding: 0.5rem 0.75rem;
-  text-decoration: none;
-  color: #ffffff;
-  font-family: sans-serif;
-  font-size: 1rem;
+  outline: 0;
   cursor: pointer;
-  text-align: center;
-  -webkit-appearance: none;
-  -moz-appearance: none;
+}
+
+.img-contain {
+  width: 250px;
+  height: 160px;
+  overflow: hidden;
+  transform-origin: 50% 50%;
+}
+
+img {
+  width: 100%;
+  transform-origin: 50% 50%;
+  cursor: pointer;
+  transform: scaleY(1) translateZ(0);
+  margin: 5px;
+}
+
+main {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 </style>
